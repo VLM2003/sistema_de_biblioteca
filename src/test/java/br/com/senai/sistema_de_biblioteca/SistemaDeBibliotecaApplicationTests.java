511951f1df444b378c0c5ec20e754f58d1f0bd5b
Your Name <you@example.com>
2026-03-06 08:17:47 -0300

    sistema de biblioteca

---
+++ src/test/java/br/com/senai/sistema_de_biblioteca/SistemaDeBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.sistema_de_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
